e5aee899e1737420181ac0d6daffb12821dc9cb9
Kenneth Horsman <125078399+KennethHorsman@users.noreply.github.com>
2023-02-28 00:57:44 +0000

    testing file handling

---
+++ Other/Challenges/File Handling.py
@@ -0,0 +1,13 @@
+test_file = open("test file.txt", "r")
+print(test_file.read)
+
+
+
+"""
+TO OPEN ELSEWHERE:
+test_file = open("/workspaces/Project-2/Other/Challenges/test file.txt", "r")
+
+TO REMOVE:
+import os
+os.remove("test file.txt")
+"""
