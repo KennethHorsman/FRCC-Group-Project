3d1aad7de684f104a97ca35d9d67370161f0d720
Kenneth Horsman <125078399+KennethHorsman@users.noreply.github.com>
2023-03-07 04:08:29 +0000

    edited bubble sort file

--- Other/Cengage Assignments/householdSize.py
+++ Other/Cengage Assignments/householdSize.py
@@ -6,35 +6,23 @@ Input:  Interactive.
 Output:  Mean and median household size.
 """
 
-# Initialize variables.
-householdSizes = []  # Array used to store household sizes.
-numSizes = 0
-total = 0.0
-mean = 0.0
-median = 0.0
+import statistics
 
-# Input household size
-householdSizeString = input("Enter household size or 999 to quit: ")
-householdSize = int(householdSizeString)
-# This is the work done in the fillArray() function
-while (householdSize != 999):
-    # Place value in array.
-    householdSizes.append(householdSize)
-    # Calculate total of household sizes
-    total = sum(householdSizes)
+householdSizes = []
+SENTINEL = False
 
-    numSizes += 1  # We have one more house
-    householdSizeString = input("Enter household size or 999 to quit: ")
-    householdSize = int(householdSizeString)
+while SENTINEL is False:
+    householdSize = int(input("Enter household size or 999 to quit: "))
+    if householdSize != 999:
+        householdSizes.append(householdSize)
+    else:
+        SENTINEL = True
 
-# Find the mean
+total = sum(householdSizes)
 mean = total / len(householdSizes)
 
-# This is the work done in the sortArray() function
 householdSizes.sort()
-# This is the work done in the displayArray() function
-import statistics
 median = statistics.median(householdSizes)
-# Find the median
+
 print(mean)
 print(median)
